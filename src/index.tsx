d3cafdc9d9a68b68354a926273948b2a716aa659
Pyunghyuk Yoo <yoophi@gmail.com>
2023-01-25 20:19:35 +0900

    UPDATE: apply typescript

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "./routes/root";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 root.render(
   <React.StrictMode>
